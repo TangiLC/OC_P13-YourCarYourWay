240e48770b82c2683a5585ef96588ba2f6d379a0
TangiLC <tangi@le-cadre.net>
2025-04-16 16:25:09 +0200

    init

--- bdd/test_populate.sql
+++ bdd/test_populate.sql
@@ -17,7 +17,7 @@ INSERT INTO dialogs (id, topic, status, created_at, closed_at) VALUES
 -- Ajout dans messages
 INSERT INTO messages (id, dialog_id, timestamp, is_read, content, sender_id) VALUES
 (1, 1, '2025-04-12T12:12:00', TRUE, 'Je souhaite de l''aide pour modifier une date de départ', 2),
-(2, 1, '2025-04-12T12:13:00', TRUE, 'Bonjour Tom, le suis Sabrina en charge de votre dossier AB123456. Pouvez-vous me donner plus de renseignements ?', 1),
+(2, 1, '2025-04-12T12:13:00', TRUE, 'Bonjour Tom, je suis Sabrina en charge de votre dossier AB123456. Pouvez-vous me donner plus de renseignements ?', 1),
 (3, 1, '2025-04-12T12:14:00', TRUE, 'J''ai fait une erreur, la date de départ sera le 18/04 au lieu du 28/04', 2),
 (4, 1, '2025-04-12T12:15:00', TRUE, 'Les modifications dans un délai supérieur à 48h ne sont pas un problème et n''engendrent aucun frais, je me charge de votre modification', 1),
 (5, 1, '2025-04-12T12:16:00', TRUE, 'Merci, quelle est la démarche ?', 2),
